f8e64d164a566d91ca0583a1e8a8ceecebb3eaf0
Lam Pham <t8phho00@students.oamk.fi>
2020-05-28 01:45:40 +0300

    add downloading data, extract from json

--- python_project/data virtualization/rw_visual.py
+++ python_project/data virtualization/rw_visual.py
@@ -17,6 +17,6 @@ ax.plot(rw.x_values, rw.y_values, linewidth=10)
 ax.scatter(0, 0, c='green', edgecolors='none', s=100)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
            s=100)
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+#ax.get_xaxis().set_visible(False)
+#ax.get_yaxis().set_visible(False)
 plt.show()
--- python_project/data virtualization/sitka_highs.py
+++ python_project/data virtualization/sitka_highs.py
@@ -1,24 +1,37 @@
 import csv
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     # Get high temperatures from this file.
-    highs = []
+    dates, highs, lows, rains = [], [], [], []
     for row in reader:
         high = int(row[5])
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        low = int(row[6])
+        rain = float(row[3])
+        rains.append(rain)
+        dates.append(current_date)
         highs.append(high)
+        lows.append(low)
+        
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.plot(dates, rains, c='green', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-     # Plot the high temperatures.
-   plt.style.use('seaborn')
-   fig, ax = plt.subplots()
-ax.plot(highs, c='red')
-
-   # Format plot.
-ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+# Format plot.
+ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
-   ax.set_ylabel("Temperature (F)", fontsize=16)
-   ax.tick_params(axis='both', which='major', labelsize=16)
-
-   plt.show()
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='y', which='major', labelsize=16)
+plt.yscale('linear')
+plt.show()
