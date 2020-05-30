c497d8922405bce278813c1c204350c23ea1f34c
Lam Pham <t8phho00@students.oamk.fi>
2020-05-30 15:17:25 +0300

    django web projects

---
+++ python_project/web_application/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\acer\desktop\python\python_summer_study\python_project\web_application\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_project/web_application/pizzaria/pizzas/urls.py
@@ -0,0 +1,13 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pizzas'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    path('pizzas/', views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
+]
